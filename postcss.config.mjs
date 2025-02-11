0fd89c135e32c772a3189061ff2864cb5ef833c1
CassieDee026 <117992982+CassieDee026@users.noreply.github.com>
2025-02-11 08:45:44 +0300

    yes

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,12 @@
+
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    '@tailwindcss/postcss': {}, 
+    'postcss-import': {},
+    'postcss-nesting': {},
     tailwindcss: {},
+    autoprefixer: {},
   },
 };
 
